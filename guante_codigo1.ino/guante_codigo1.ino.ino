9b4275abb1fe60156246cc7e731b343848081360
CarmenGuidet <cguidet98@gmail.com>
2018-08-05 23:44:05 +0200

    Codigo
    Version 0.2

--- guante_codigo1.ino/guante_codigo1.ino.ino
+++ guante_codigo1.ino/guante_codigo1.ino.ino
@@ -60,54 +60,84 @@ int int_menique=digitalRead(menique);
      */
     case 10000://el pulgar (P) es LOW
       tone(jack,262);//suena la nota C4(do)
+      delay(1000); //espera un segundo es decir 1000ms
+      noTone(3);//deja de sonar la nota
       break;
     case 1000://el indice (I) es LOW
       tone(jack,294);//suena la nota D4(re)
+      delay(1000); 
+      noTone(3);
       break;
     case 100://el corazon (C) es LOW
       tone(jack,330);//suena la nota E4(mi)
+      delay(1000); 
+      noTone(3);
       break;
     case 10://el anular (A) es LOW
       tone(jack,349);//suena la nota F4(fa)
+      delay(1000); 
+      noTone(3);
       break;
     case 1:// el menique( M) es LOW
       tone(jack,392);//suena la nota G4(sol)
+      delay(1000); 
+      noTone(3);
       break;
     case 11000: // P & I
       tone(jack,440);//suena la nota A4 (la)
+      delay(1000); 
+      noTone(3);
       break;
     case 10100: // C & P
       tone(jack,494);//suena la nota B4(si)
+      delay(1000); 
+      noTone(3);
       break;
    /*****
     * Empieza una nueva escala 
     ****/
     case 10010://A & P 
       tone(jack,523);//suena la nota C5 do
+      delay(1000); 
+      noTone(3);
       break;
     case 10001://M & P son LOW
       tone(jack,587);//suena la nota D5 re 
+      delay(1000); 
+      noTone(3);
       break;
     case 1100://C & I
       tone(jack,659);//suena la nota E5 mi 
+      delay(1000); 
+      noTone(3);
       break;
     case 1010://A & I
       tone(jack,698);//suena la nota F5 fa
+      delay(1000); 
+      noTone(3);
       break;
     case 1001:// M & I son LOW
       tone(jack,784);//suena la nota G5 sol
+      delay(1000); 
+      noTone(3);
       break;
     case 110://A & C 
       tone(jack,880);//suena la nota A5 la 
+      delay(1000); 
+      noTone(3);
       break;
     case 101:// M & C son LOW
       tone(jack,988);//suena la nota B5 si
+      delay(1000); 
+      noTone(3);
       break;
     /***
      * Nueva escala 
      */
     case 11://M & A son LOW
       tone(jack,1047);//suena la nota C6 do
+      delay(1000); 
+      noTone(3);
       break;
 
     default:// No ha ocurrido ninguna de las anteriores combinaciones 
